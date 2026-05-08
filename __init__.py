14995f6738bd49fc41f0dbffcb8cf349f4de21f2
Defani Arman Alfitriansyah <89701340+Defani@users.noreply.github.com>
2026-05-08 13:00:58 +0700

    Delete __init__.py

--- __init__.py
+++
@@ -1,8 +0,0 @@
-# RasterViz— Scientific Raster Visualization for QGIS
-# License: GNU GPL v2 or later
-# https://github.com/Defani/QRVIZ
-
-
-def classFactory(iface):
-    from .qrviz import QRVIZPlugin
-    return QRVIZPlugin(iface)
